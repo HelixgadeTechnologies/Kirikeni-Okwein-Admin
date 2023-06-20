bcaf38db48e0d685af4323ea1c4b0bc0e4ff86c5
emmanueltech2019 <emmanueltech2019@gmail.com>
2023-06-20 14:51:20 +0100

    New updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import Sidebar from './SideBar';
+import Sidebar from '@/components/SideBar';
 import TopBar from './TopBar';
 
 const Layout = ({ children, isActivePage }) => {
